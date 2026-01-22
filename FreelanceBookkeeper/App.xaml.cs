01aa5e607ade04bee02f81c2eefea11da7388fdd
DESKTOP-QOKT43B\Red <jovefernandez@gmail.com>
2026-01-22 19:52:53 +0100

    Added new base project

---
+++ FreelanceBookkeeper/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FreelanceBookkeeper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
