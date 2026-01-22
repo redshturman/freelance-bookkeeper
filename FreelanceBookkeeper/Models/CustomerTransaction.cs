77c018219a457f13f960c5555a6ecf3749aaaa8d
DESKTOP-QOKT43B\Red <jovefernandez@gmail.com>
2026-01-22 19:53:58 +0100

    Added new models

---
+++ FreelanceBookkeeper/Models/CustomerTransaction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreelanceBookkeeper.Models
+{
+    /// <summary>
+    /// Represents the customer transaction.
+    /// </summary>
+    public class CustomerTransaction
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the customer transaction.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the customer.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the identification number of the customer.
+        /// </summary>
+        public string? Identification { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the address of the customer.
+        /// </summary>
+        public string Address { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the phone number of the customer.
+        /// </summary>
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the unique number assigned to the invoice.
+        /// </summary>
+        public int InvoiceNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total amount of the invoice. Tax included.
+        /// </summary>
+        public decimal TotalAmount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date when the invoice was issued.
+        /// </summary>
+        public DateOnly InvoiceDate { get; set; }
+    }
+}
---
+++ FreelanceBookkeeper/Models/Expense.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreelanceBookkeeper.Models
+{
+    /// <summary>
+    /// Represents an expense for a freelance.
+    /// </summary>
+    public class Expense
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the expense.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the he supplier name associated with the expense.
+        /// </summary>
+        public string SupplierName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the supplier identification number associated with the expense.
+        /// </summary>
+        public string SupplierId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the description of the expense.
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the total amount of the expense. Tax included.
+        /// </summary>
+        public decimal TotalAmount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date when the expense was incurred.
+        /// </summary>
+        public DateOnly ExpenseDate { get; set; }
+    }
+}
