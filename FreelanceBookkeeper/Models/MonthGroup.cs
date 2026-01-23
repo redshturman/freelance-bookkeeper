321046183fb6ff6bb823f0750f1f405d71ac1552
DESKTOP-QOKT43B\Red <jovefernandez@gmail.com>
2026-01-23 16:37:42 +0100

    Solved issue where the filtering didn't work properly

---
+++ FreelanceBookkeeper/Models/MonthGroup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreelanceBookkeeper.Models
+{
+    /// <summary>
+    /// Represents a group of months for filtering.
+    /// </summary>
+    public class MonthGroup
+    {
+        /// <summary>
+        /// The actual group of months.
+        /// </summary>
+        public int Group { get; set; }
+        /// <summary>
+        /// The name of the group of months to show.
+        /// </summary>
+        public string Display { get; set; } = string.Empty;
+    }
+}
